e86332b18693d5e27816e9ec9067acbaf02ec7dd
nestormarlier <nestor.mar@gmail.com>
2023-10-19 08:57:11 -0300

    modificaciones varias

--- tareas/models.py
+++ tareas/models.py
@@ -24,18 +24,18 @@ class Task(models.Model):
         ('PREDICTIVA', 'Predictiva')
     )
 
-        user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks_created', verbose_name='Usuario')
+        user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks_created', verbose_name='Usuario Crea')
         maquina = models.ForeignKey(Activo,on_delete=models.CASCADE, null=True, blank=True)
         tipo = models.CharField(max_length=10, choices=TIPO_TAREA)
         description = models.TextField(verbose_name='Descripción')
         priority = models.CharField(max_length=10,default='ALTA',choices=ORDEN_PRIORIDAD,verbose_name='Prioridad')
         state = models.CharField(max_length=50, null=True, blank=True, choices=ESTADO_TAREA, verbose_name='Estado')
-        created_at = models.DateTimeField(auto_now_add=True, verbose_name='Tarea creada')
-        completed_at = models.DateTimeField(null=True, blank=True, verbose_name='Fecha completada')
-        completed_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,related_name='tasks_completed',verbose_name='Usuario que realizo la tarea')
+        created_at = models.DateTimeField(auto_now_add=True, verbose_name='Creada')
+        completed_at = models.DateTimeField(null=True, blank=True, verbose_name='Completada')
+        completed_user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True,related_name='tasks_completed',verbose_name='Usuario Realizo')
 
         def __str__(self):
-            return str(self.id)
+            return str(self.id) + '-' + self.description
         
         class Meta:
             verbose_name_plural = 'Tareas'
