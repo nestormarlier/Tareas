c7d7a61d2d5b7adab67cd57dd7d89e57a9c5984c
nestormarlier <nestor.mar@gmail.com>
2024-02-19 12:23:57 -0300

    navbar en home/

--- tareas/urls.py
+++ tareas/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import tarea_x_estado
+from .views import tarea_x_estado, home
 
 urlpatterns = [
-    path('', tarea_x_estado, name="tareas")
+    path('', tarea_x_estado, name="tareas"),
+    path('home/', home, name="home")
 ]
