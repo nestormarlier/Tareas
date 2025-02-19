9dd8854679b8e9fa950cf82a78313f4c46cd1413
nestormarlier <nestor.mar@gmail.com>
2025-02-19 11:59:32 -0300

    Actualizado con días de retraso

--- tareas/admin.py
+++ tareas/admin.py
@@ -1,37 +1,11 @@
 from django.contrib import admin
-from django.urls import reverse #crear boton grafico
-from django.http import HttpResponseRedirect # direccionar boton grafico
 from .models import Activo,Task
 from django.utils import timezone
 from django.utils.html import format_html
 from django_admin_multi_select_filter.filters import MultiSelectFieldListFilter
-# from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
-
-# class TaskResource(resources.ModelResource):
-#     class Meta:
-#         model = Task
-# @admin.register(Task)
-
 class TaskAdmin(ImportExportModelAdmin,admin.ModelAdmin):
-    list_display = ('maquina','description','created_at','dias_transcurridos','tipo','priority_color_css', 'state_color_css', 'user', 'completed_user')
-    list_filter = (('state',MultiSelectFieldListFilter), 'tipo','priority',)
-    actions = ['dias_transcurridos_mod']
-
-     # Especifica los valores iniciales por defecto para el filtro de selección múltiple
-    list_filter[0][1].default_value = ['PENDIENTE', 'PROCESO']
-
-    def dias_transcurridos_mod(self, request, queryset):
-        nuevos_valores = 42  # Cambia esto al valor deseado
-
-        for objeto in queryset:
-           objeto.dias_transcurridos = nuevos_valores
-           objeto.save()
-           print(f"Objeto {objeto.id} actualizado a {nuevos_valores}")
-
-    dias_transcurridos_mod.short_description = "Cambiar el valor de 'mi_entero' a 42"
-
     exclude = ('user',)  # Excluye el campo "usuario" del formulario de administración
     readonly_fields = ('user','created_at','completed_at', 'completed_user',)  # Hace que el campo "usuario" sea de solo lectura
 
@@ -44,10 +18,14 @@ class TaskAdmin(ImportExportModelAdmin,admin.ModelAdmin):
             return 'baja-priority'
         return ''
 
-    # priority_color.short_description = 'Prioridad'
+    priority_color.short_description = 'Prioridad'
 
     def priority_color_css(self, obj):
         return format_html('<span class="{}">{}</span>', self.priority_color(obj), obj.get_priority_display())
+
+
+    priority_color_css.allow_tags = True
+    priority_color_css.short_description = 'Prioridad'
 
     def state_color(self, obj):
         if obj.state == 'PENDIENTE':
@@ -63,21 +41,24 @@ class TaskAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 
     state_color_css.allow_tags = True
     state_color_css.short_description = 'Estado'
-    
-    priority_color_css.allow_tags = True
-    priority_color_css.short_description = 'Prioridad'
+
+    def custom_created_at(self, obj): # Solo fecha sin hora
+        return obj.created_at.date() if obj.created_at else None
+
+    custom_created_at.short_description = 'FECHA PEDIDO'
+
+    def days_since_created(self, obj):
+        if obj.created_at:
+            today = timezone.now().date()
+            delta = today - obj.created_at.date()
+            return delta.days
+        return None
+
+    days_since_created.short_description = 'DÍAS PENDIENTES'
 
     search_fields = ['maquina__nombre']
-
-    # def custom_created_at(self, obj): # Solo fecha sin hora
-    #     return obj.created_at.date() if obj.created_at else None
-
-    # custom_created_at.short_description = 'FECHA PEDIDO'
-
-    # # days_since_created.short_description = 'DÍAS TRANSCURRIDOS'
-    # days_since_created.admin_order_field = 'DÍAS TRANSCURRIDOS'
-    
-    
+    list_display = ('maquina','description','custom_created_at','days_since_created','tipo','priority_color_css', 'state_color_css', 'user', 'completed_user')
+    list_filter = (('state',MultiSelectFieldListFilter), 'tipo','priority',)
 
     def has_delete_permission(self, request, obj=None):
         if obj and obj.state == 'COMPLETADA':
@@ -86,10 +67,10 @@ class TaskAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         if not change:  # Si es una nueva tarea
-            obj.user = request.user  # Asigna el usuario logueado
-            if 'state' in form.changed_data and form.cleaned_data['state'] == 'COMPLETADA':
-                obj.completed_user = request.user
-                obj.completed_at = timezone.now()  # Establece la fecha actual como fecha de finalización
+             obj.user = request.user  # Asigna el usuario logueado
+             if 'state' in form.changed_data and form.cleaned_data['state'] == 'COMPLETADA':
+                 obj.completed_user = request.user
+                 obj.completed_at = timezone.now()  # Establece la fecha actual como fecha de finalización
         else:
             if 'state' in form.changed_data and form.cleaned_data['state'] == 'COMPLETADA':
                 obj.completed_user = request.user
@@ -100,12 +81,9 @@ class TaskAdmin(ImportExportModelAdmin,admin.ModelAdmin):
         if obj and obj.state == 'COMPLETADA':
             return self.readonly_fields + ('maquina','tipo','description', 'priority', 'state')
         return self.readonly_fields
-    
-    
 
 admin.site.site_header = 'Sistema de gestión de tareas'
 admin.site.index_title = 'Área de características'                 # default:
 admin.site.site_title = 'Sistema de gestión' # default: "Django site admin"
-
 admin.site.register(Task,TaskAdmin)
 admin.site.register(Activo)
